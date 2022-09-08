a93d45f1781ac69313d631c7c4dc384024a3f6d4
Aisha <mudathiraisha657@gmail.com>
2022-09-08 16:03:06 -0700

    first string output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-*main - prints the string
-*Return: 0 if exited properly, non-zero otherwise
+* main - prints the string
+* Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
