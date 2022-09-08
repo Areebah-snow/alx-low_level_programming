458aa21e81237735cd43b3506ef769b56753747e
Aisha <mudathiraisha657@gmail.com>
2022-09-08 15:53:24 -0700

    first string output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  *main - prints the string 
  *Return: 0 if exited properly, non-zero otherwise
- */
+*/
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 
 }
