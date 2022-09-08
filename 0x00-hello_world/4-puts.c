52e80b619f26449bfd994ce7a59aae9dd4362dc4
Aisha <mudathiraisha657@gmail.com>
2022-09-08 15:42:38 -0700

    first string output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main(){
+
+/**
+ *  * main - prints the string in the bracket
+ *  Return: 0 if exited properly, non-zero otherwise
+ *    */
+int main(void)
+{
    puts("Programming is like building a multilingual puzzle.");
-   return 0;
+   return (0);
 
 }
