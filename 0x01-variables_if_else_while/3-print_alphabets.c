1973edb6b71d31aa493f45fd7daa4a6c78df5689
Aisha <mudathiraisha657@gmail.com>
2022-09-09 16:28:00 -0700

    display big and small letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+int letter;
+for(letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+for(letter = 'A'; letter <= 'Z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
