8f4d4ec624e0e816f57fcb1f1b64283abf9abfe4
Aisha <mudathiraisha657@gmail.com>
2022-09-09 16:59:40 -0700

    display number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - displays whole digit
+* Return: 0 if successful
+*/
+
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+printf("%d", n);
+printf("\n");
+return (0);
+}
