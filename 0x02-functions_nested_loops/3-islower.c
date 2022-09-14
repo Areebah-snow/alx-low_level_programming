be56d76ca3076884270d989fbbc302e34dac73d8
Aisha <mudathiraisha657@gmail.com>
2022-09-14 08:33:01 -0700

    3..

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - CHECK FOR LOWER CASE LETTERS
+ * @C : CHARACTER TO CHECK THE CASE
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
