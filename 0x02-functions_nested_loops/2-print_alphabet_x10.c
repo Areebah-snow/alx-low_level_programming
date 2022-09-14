8de4d65456c0bff94421b1275c9bf4edc10e32da
Aisha <mudathiraisha657@gmail.com>
2022-09-14 08:24:00 -0700

    2-x_10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	int a = 0;
+
+	while (a  < 10)
+	{
+		c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+		i++
+	}
+}
+
