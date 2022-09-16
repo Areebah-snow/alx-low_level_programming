274cfdc9c7d2d0eeddd742aac2c82394a67efdf3
Aisha <mudathiraisha657@gmail.com>
2022-09-16 06:49:15 -0700

    print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,6 @@
  *  print_most_numbers-  prints the numbers, from 0 to 9, followed by a new line
  *  Return: 0
  */
-
 void  print_most_numbers(void)
 {
 	int i;
@@ -12,7 +11,7 @@ void  print_most_numbers(void)
 	{
 		if (i != 2 && i != 4)
 		{
-			_putchar(i + '0');
+			_putchar((i % 10) + '0');
 		}
 	}
 	_putchar('\n');
