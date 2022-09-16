3bafa50c36cf2b88de448f34c0867dc78479796c
Aisha <mudathiraisha657@gmail.com>
2022-09-15 20:46:22 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,8 +5,8 @@
  */
 void print_numbers(void)
 {
- int i;
-   for (i = 0; i <= 9; i++)
+ 	int i;
+   	for (i = 0; i <= 9; i++)
         _putchar((i % 10) + '0');
-  _putchar('\n');
+  	_putchar('\n');
 }
