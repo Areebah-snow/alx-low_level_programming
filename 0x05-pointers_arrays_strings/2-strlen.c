1c8936c61323b24478e3c72aec0176921a380921
Aisha <mudathiraisha657@gmail.com>
2022-09-19 16:26:07 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * int _strlen - Entry point
+ * @s: pointer with a character datatype
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++
+	}
+	return (i);
+}
